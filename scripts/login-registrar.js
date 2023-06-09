884b5c5b066b4244380600ca138854230c913543
Iuri <iuriwerneck10@gmail.com>
2023-06-08 22:20:06 -0300

    tela de login e registrar prontas. Faltando colocar a logo e se preferir alguns icones de dinheiro, ou algo similar.

---
+++ scripts/login-registrar.js
@@ -0,0 +1,21 @@
+const wrapper = document.querySelector('.wrapper');
+const linkLogin = document.querySelector('.link-login');
+const linkRegistrar = document.querySelector('.link-registrar');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconeFechar = document.querySelector('.icone-fechar');
+
+linkRegistrar.addEventListener('click', ()=> {
+    wrapper.classList.add('ativado');
+});
+
+linkLogin.addEventListener('click', ()=>{
+    wrapper.classList.remove('ativado');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('popup-ativado');
+});
+
+iconeFechar.addEventListener('click', ()=> {
+    wrapper.classList.remove('popup-ativado');
+});
