c9ad919c0ddf225755708cc8fe0dc239c326cf56
iuri <iuriwerneck10@gmail.com>
2023-06-26 15:49:57 -0300

    sobre atualizado

--- sobre.php
+++ sobre.php
@@ -7,8 +7,43 @@
     <link rel="stylesheet" href="./css/style.css">
     <link rel="shortcut icon" href="./imgs/bitcoin-aceito.png" type="image/x-icon">
     <title>Sobre</title>
+    <style>
+        .container {
+            width: 700px;
+            height: 400px;
+            position: absolute;
+            padding-left: 20px;
+            padding-right: 20px;
+            border-radius: 50px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            backdrop-filter: blur(10px);
+        }
+        hr{
+            width: 80%;
+            color: #343838;
+            border-top: 3px solid #343838;
+        }
+
+        @media screen and (max-width: 700px) {
+            .container {
+                width: 90%;
+            }
+        }
+    </style>
 </head>
 <body>
-    
+    <div class="container">
+        <h2>CEFET MONEY- O Banco do CEFET</h2>
+        <hr>
+        <p>
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+        </p>
+    </div>
 </body>
 </html>
