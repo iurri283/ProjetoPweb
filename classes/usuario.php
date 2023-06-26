672acc95f040d377eba2d4cae9df95d3c2ab4b7e
gabriel <leirbagrp@gmail.com>
2023-06-26 13:42:18 -0300

    login

--- classes/usuario.php
+++ classes/usuario.php
@@ -25,13 +25,40 @@
             $this->rua = $rua;
             $this->numero = $numero;
             $this->complemento = $complemento;
-
-            $conn = new Conn();
-
-            $query = "INSERT INTO Usuario(nomeUsuario, cpfUsuario, nascUsuario, senhaUsuario, emailUsuario, cidadeUsuario, bairroUsuario, ruaUsuario, numeroUsuario, compleUsuario) VALUES ('".$this->nome."', '".$this->cpf."', '".$this->dataNasc."', '".$this->senha."', '".$this->email."', '".$this->cidade."', '".$this->bairro."', '".$this->rua."', '".$this->numero."', '".$this->complemento."')";
             
-            $result = $conn->conectar()->prepare($query);
-            $result->execute();
+            $conn = new Conn();
+            $pdo = $conn->conectar();
+
+            // Consulta para verificar se o usuário já existe
+            $sql = "SELECT * FROM Usuario WHERE cpfUsuario = :cpf";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':cpf', $this->cpf);
+            $stmt->execute();
+
+            // Verificar se o usuário já está cadastrado
+            if ($stmt->rowCount() > 0) {
+                echo "<script>alert('CPF já cadastrado!'); window.location.href = '../index.php';</script>";
+            } else {
+                // Inserir o novo usuário no banco de dados
+                $sql = "INSERT INTO Usuario (nomeUsuario, cpfUsuario, nascUsuario, senhaUsuario, emailUsuario, cidadeUsuario, bairroUsuario, ruaUsuario, numeroUsuario, compleUsuario) VALUES (:nome, :cpf, :dataNasc, :senha, :email, :cidade, :bairro, :rua, :numero, :complemento)";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(':nome', $this->nome);
+                $stmt->bindParam(':cpf', $this->cpf);
+                $stmt->bindParam(':dataNasc', $this->dataNasc);
+                $stmt->bindParam(':senha', $this->senha);
+                $stmt->bindParam(':email', $this->email);
+                $stmt->bindParam(':cidade', $this->cidade);
+                $stmt->bindParam(':bairro', $this->bairro);
+                $stmt->bindParam(':rua', $this->rua);
+                $stmt->bindParam(':numero', $this->numero);
+                $stmt->bindParam(':complemento', $this->complemento);
+
+                if ($stmt->execute()) {
+                    echo "<script>alert('Usuário cadastrado com sucesso!'); window.location.href = '../index.php';</script>";
+                } else {
+                    echo "<script>alert('Erro ao cadastrar usuário!'); window.location.href = '../index.php';</script>";
+                }
+            }
         }
 
         public function login($cpf, $senha)
@@ -42,28 +69,27 @@
             $conn = new Conn();
             $pdo = $conn->conectar();
 
-            try {
-                $query = "SELECT * FROM Usuario WHERE cpfUsuario = :cpf AND senhaUsuario = :senha";
-                $stmt = $pdo->prepare($query);
-                $stmt->bindParam(':cpf', $this->cpf);
-                $stmt->bindParam(':senha', $this->senha);
-                $stmt->execute();
+            $query = "SELECT * FROM Usuario WHERE cpfUsuario = :cpf AND senhaUsuario = :senha";
+            $stmt = $pdo->prepare($query);
+            $stmt->bindParam(':cpf', $this->cpf);
+            $stmt->bindParam(':senha', $this->senha);
+            $stmt->execute();
 
-                $user = $stmt->fetch(PDO::FETCH_ASSOC);
-                if ($user) {
-                    // Autenticação bem-sucedida
-                    session_start();
-                    $_SESSION['cpf'] = $user['cpfUsuario'];
-                    // Redirecionar para página de sucesso ou página restrita
-                    header("Location: ../home.php");
-                    exit();
-                } else {
-                    // Autenticação falhou
-                    echo "<script>alert('Usuário ou senha inválidos!'); window.location.href = '../index.php';</script>";
-                }
-            } catch (PDOException $e) {
-                echo "Erro ao conectar com o banco de dados: " . $e->getMessage();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($user) 
+            {
+                // Autenticação bem-sucedida
+                session_start();
+                $_SESSION['cpf'] = $user['cpfUsuario'];
+                // Redirecionar para página de sucesso ou página restrita
+                header("Location: ../home.php");
+                exit();
+            } else 
+            {
+                // Autenticação falhou
+                echo "<script>alert('Usuário ou senha inválidos!'); window.location.href = '../index.php';</script>";
             }
+            
         }
     }
 ?>
